8f2bebba33b06b182db15c63a009398b3d16760b
wafiullah <wafiullahmayar@gmail.com>
2021-04-01 22:41:22 +0430

    added vee validate library to contuct.vue component form

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,11 @@ require('./bootstrap');
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import carousel from 'vue-owl-carousel'
+import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm'
+import ValidationObserver from 'vee-validate'
+
+Vue.component(' ValidationProvider' ,  ValidationProvider);
+Vue.component(' ValidationObserver' ,  ValidationObserver);
 
 
 import routes from './router'
--- resources/js/views/Contact.vue
+++ resources/js/views/Contact.vue
@@ -2,9 +2,9 @@
    
     <div class="contact-area mtb-60px">
                 <div class="container">
-                    <div class="contact-map mb-10">
+                    <!-- <div class="contact-map mb-10">
                         <div id="mapid"></div>
-                    </div>
+                    </div> -->
                     <div class="custom-row-2">
                         <div class="col-lg-4 col-md-5">
                             <div class="contact-info-wrap">
@@ -64,23 +64,44 @@
                                 <div class="contact-title mb-30">
                                     <h2>Get In Touch</h2>
                                 </div>
-                                <form class="contact-form-style" id="contact-form" action="https://htmldemo.hasthemes.com/ecolife-preview/ecolife/assets/php/mail.php" method="post">
+                                <ValidationObserver v-slot="{ handleSubmit}">  
+                                <form @submit.prevent="handleSubmit(onSubmit)" class="contact-form-style" id="contact-form" action="https://htmldemo.hasthemes.com/ecolife-preview/ecolife/assets/php/mail.php" method="post">
+                               
                                     <div class="row">
+                                         <ValidationProvider name="Name" rules="required|alpha" v-slot="{ errors }">
                                         <div class="col-lg-6">
-                                            <input name="name" placeholder="Name*" type="text" />
+                                            <input v-model="name" placeholder="Name*" type="text" class="form-control" />
+                                            
                                         </div>
+                                        <span> {{errors[0]}} </span>
+                                        </ValidationProvider>
+                                        <ValidationProvider name="Email" rules="required|email" v-slot="{ errors }">
                                         <div class="col-lg-6">
-                                            <input name="email" placeholder="Email*" type="email" />
+                                            <input v-model="email" placeholder="Email*" type="email" class="form-control" />
+                                            
                                         </div>
-                                        <div class="col-lg-12">
-                                            <input name="subject" placeholder="Subject*" type="text" />
+                                        <span> {{errors[0]}} </span>
+                                        </ValidationProvider>
+
+                                        <ValidationProvider name="Subject" rules="required|min:5" v-slot="{ errors }">
+                                        <div class="col-lg-6">
+                                            <input v-model="subject" placeholder="Subject*" type="text" class="form-control" />
+                                           
                                         </div>
-                                        <div class="col-lg-12">
-                                            <textarea name="message" placeholder="Your Message*"></textarea>
-                                            <button class="submit" type="submit">SEND</button>
+                                         <span> {{errors[0]}} </span>
+                                        </ValidationProvider>
+
+                                        <ValidationProvider name="Message" rules="required|min:10" v-slot="{ errors }">
+                                        <div class="col-lg-6">
+                                            <input v-model="message" placeholder="Message*" type="text" class="form-control" />
+                                            
                                         </div>
+                                        <span> {{errors[0]}} </span><br>
+                                        </ValidationProvider>
+                                        <input type="submit" value="Send" class="btn btn-primary mt-3">
                                     </div>
                                 </form>
+                                </ValidationObserver>
                                 <p class="form-messege"></p>
                             </div>
                         </div>
@@ -88,3 +109,24 @@
                 </div>
             </div>
 </template>
+
+<script>
+export default {
+    name: "Contact",
+    data() {
+        return {
+            formData: {
+            name: '',
+            eamil: '',
+            subject: '',
+            message: '',
+            }
+        }
+    },
+    methods: {
+        onSubmit(){
+            console.log(this.formData);
+        }
+    },
+}
+</script>
