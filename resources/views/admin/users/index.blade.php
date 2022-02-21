ef4e4bc6025a9b7d86597cba3db138379eef5357
Muhammad Shoaib <mshoaibdev@gmail.com>
2022-02-22 00:06:22 +0500

    users datatable

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -44,7 +44,7 @@
                 <a href="{{ route('admin.users.create') }}" class="btn btn-primary mg-b-20 mg-l-20">Add
                     Customer</a>
                 <div class="table-wrapper">
-                    <table class="table display table-condensed table-sm">
+                    <table class="table display table-condensed table-sm" id="datatable1">
                         <thead>
                             <tr>
                                 <th>Name</th>
@@ -81,41 +81,36 @@
 @endsection
 @push('js')
 <script src="{{asset('assets/lib/datatables/js/datatables.min.js')}}"></script>
-<script src="{{asset('assets/js/dataTable.js')}}"></script>
-<!-- Modal -->
-{{-- <div class="modal fade" id="blockAccount" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <form action="{{route('admin.user.block.account')}}" onsubmit="return confirm('are you sure?')" method="POST">
-            <div class="modal-content" style="width: 417px;">
-                @csrf
-                <div class="modal-header">
-                    <h5 class="modal-title">Block Account</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <input type="hidden" name="userId" class="userId">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Block Account</button>
-                </div>
-            </div>
-        </form>
-    </div>
-</div> --}}
 <script>
-    $('#blockAccount').on('show.bs.modal', event => {
-        var button = $(event.relatedTarget);
-
-        var userId = button.data('id');
-        $('.userId').val(userId);
-
+    $('#datatable1').DataTable({
+    responsive: true,
+    dom: 'Bfrtip',
+    buttons: [{
+    extend: 'csvHtml5',
+    exportOptions: {
+    columns: [0, ':visible']
+    }
+    },
+    {
+    extend: 'excelHtml5',
+    exportOptions: {
+    columns: ':visible'
+    }
+    },
+    {
+    extend: 'pdf',
+    exportOptions: {
+    columns: ':visible'
+    }
+    },
+    'colvis'
+    ],
+    language: {
+    searchPlaceholder: 'Search...',
+    sSearch: '',
+    lengthMenu: '_MENU_ items/page',
+    }
     });
-
 </script>
 
 @endpush
